75b9f89d53e7e1c4a8822d003e2b8d9abb7b97f3
vel-block1 <marcellones.vellkingjasper@dnsc.edu.ph>
2023-05-19 20:11:37 +0800

    added T&H displaying functionality

--- src/Content/Content.jsx
+++ src/Content/Content.jsx
@@ -1,25 +1,112 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import "./Content.css";
 import { ThemeContext } from "../ThemeContext";
 
 import Analytics from "../Components/AnalyticsTemplate/Analytics";
-import Header from "../Components/HeaderTemplate/Header";
-
-import { analytics } from "../Firebase";
+import firebase from "../Firebase";
+import {
+  getDatabase,
+  ref,
+  onValue,
+  orderByKey,
+  limitToLast,
+} from "firebase/database";
 const Content = () => {
-  const [username, setusername] = useState();
   const { DarkTheme } = useContext(ThemeContext);
   const [value_i] = useState(Math.floor(Math.random() * 100));
-  const [value_i_offset] = useState(315 - (value_i / 100) * 315);
 
   const [value_ii] = useState(Math.floor(Math.random() * 100));
-  const [value_ii_offset] = useState(315 - (value_ii / 100) * 315);
-  // Date
-  // const date = new Date();
-  // const day = date.toLocaleDateString("en-US", { day: "numeric" });
-  // const month = date.toLocaleDateString("en-US", { month: "long" });
-  // const year = date.toLocaleDateString("en-US", { year: "numeric" });
-  // Date
+
+  const [temperature, setTemperature] = useState("");
+  const [humidity, setHumidity] = useState("");
+
+  const realtimeDB = getDatabase();
+
+  const [date, setDate] = useState("");
+  const [id, setId] = useState("");
+
+  const [hasExecuted, setHasExecuted] = useState(false);
+
+  useEffect(() => {
+    const getCurrentDate = async () => {
+      const currentDate = new Date();
+      // Format the date to match the Firebase database structure (YYYY-MM-DD)
+      const year = currentDate.getFullYear();
+      const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
+      const day = currentDate.getDate().toString().padStart(2, "0");
+      const formattedDate = `${year}-${month}-${day}`;
+      setDate(formattedDate);
+    };
+    getCurrentDate();
+  });
+  useEffect(() => {
+    const getCurrentData = async () => {
+      const getDateOnTemp = ref(realtimeDB, "2023-05-18");
+      // const getDateOnTemp = ref(realtimeDB, ${date});
+
+      onValue(getDateOnTemp, (snapshot) => {
+        if (snapshot.exists()) {
+          let hasExecuted = false;
+
+          snapshot.forEach((childSnapshot) => {
+            if (!hasExecuted) {
+              const childKey = childSnapshot.key;
+              const childData = childSnapshot.val();
+              const childHumidity = childData.Humd;
+              const childTemperature = childData.Temp;
+
+              // console.log("Child Node:", childKey);
+              // console.log("Humidity:", childHumidity);
+              // console.log("Temperature:", childTemperature);
+
+              setTemperature(childTemperature);
+              setHumidity(childHumidity);
+              hasExecuted = true;
+            }
+          });
+        } else {
+          console.log("No child nodes found.");
+        }
+      });
+    };
+
+    getCurrentData();
+  });
+  // useEffect(() => {
+  //   const getCurrentData = async () => {
+  //     // const getDateOnTemp = firebase.getDatabase().ref(formattedDate);
+  //     const getDateOnTemp = ref(realtimeDB, "2023-05-18");
+
+  //     // const query = getDateOnTemp.orderByKey().limitToLast(1);
+
+  //     // console.log(query);
+
+  //     onValue(getDateOnTemp, (snapshot) => {
+  //       // if (snapshot.exists()) {
+  //       snapshot.forEach((childSnapshot) => {
+  //         const childKey = childSnapshot.key;
+  //         const childData = childSnapshot.val();
+  //         const childHumidity = childData.Humd;
+  //         const childTemperature = childData.Temp;
+
+  //         console.log("Child Node:", childKey);
+  //         console.log("Humidity:", childHumidity);
+  //         console.log("Temperature:", childTemperature);
+
+  //         setTemperature(childTemperature);
+  //         setHumidity(childHumidity);
+  //       });
+  //       // } else {
+  //       //   console.log("No child nodes found.");
+  //       // }
+  //       // const data = snapshot.val();
+  //       // setTemperature(data.Temp);
+  //       // setHumidity(data.Humd);
+  //     });
+  //   };
+
+  //   getCurrentData();
+  // });
 
   return (
     <div className={`content ${DarkTheme && "dark"}`}>
@@ -31,7 +118,8 @@ const Content = () => {
         <span className="last-monitor">as of May 15, 2023 10 am</span>
         <div className="divider"></div>
         <h1 className="txt-temp" id="temp">
-          34°C
+          {temperature}
+          <span className="unit">°C</span>
         </h1>
       </div>
       <div className="row header">
@@ -39,7 +127,7 @@ const Content = () => {
         <span className="last-monitor">as of May 15, 2023 10 am</span>
         <div className="divider"></div>
         <h1 className="txt-humid" id="humid">
-          43%
+          {humidity} <span className="units">%</span>
         </h1>
       </div>
       <span className="section-title">Brief Overview</span>
@@ -59,11 +147,6 @@ const Content = () => {
             <span className="last-monitor">May 15, 2023</span>
             <div className="divider"></div>
             <h1 className="txt-temp">43g-40g</h1>
-            {/* <CircularProgress
-              color="#85def6"
-              value={value_ii}
-              offset={value_ii_offset}
-            /> */}
           </div>
         </section>
       </div>
--- src/Firebase.js
+++ src/Firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
+import { getDatabase } from "@firebase/database";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,7 +23,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export default initializeApp(firebaseConfig);
 export const analytics = getAnalytics();
 export const auth = getAuth();
 export const db = getFirestore();
+export const rdb = getDatabase();
